3563d54eb0128e5ea16b00dd450a3b65417adf4e
Emithen <86219540+Emithen@users.noreply.github.com>
2025-09-01 04:03:51 +0900

    init: vite react ts tailwindcss prettier gitattributes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-20 items-center justify-center bg-blue-500 text-3xl text-red-500">
+        테일윈드 테스트
+      </div>
+    </>
+  );
+}
+
+export default App;
