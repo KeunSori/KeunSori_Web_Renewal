04f103ef619a16b9c0edbc7b785504752049ed98
Emithen <86219540+Emithen@users.noreply.github.com>
2026-02-11 00:26:50 +0900

    chore: 테스트 코드 제거

--- src/pages/home/screen/HomePage.tsx
+++ src/pages/home/screen/HomePage.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react';
-import { Calendar } from '../../../shared/component/calendar/Calendar';
 import FormInput from '../../../shared/component/FormInput';
-import { ReservationCard } from '../../../shared/component/ReservationCard';
 
 const HomePage = () => {
-  const [date, setDate] = useState(new Date());
   return (
     <div className="flex flex-col gap-3">
       <div>HomePage</div>
@@ -13,14 +9,6 @@ const HomePage = () => {
       <FormInput
         placeholder="현재 비밀번호"
         className="h-[40px] w-[170px] text-[14px] focus:border-[1px]"
-      />
-      <Calendar value={date} onChange={setDate} disableDates={[new Date(2026, 1, 9)]} />
-      <ReservationCard
-        name="김지상"
-        instrumentValue="드럼"
-        dateValue="2026년 2월 13일"
-        timeValue="16:00 - 17:00"
-        confirmState={false}
       />
     </div>
   );
